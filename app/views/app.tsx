f585ed7e8a599f9f9a47669a5503372d8bf474d7
Phil Hendricks <pfhendri1@gmail.com>
2025-04-25 17:50:04 -0500

    moved home to external page file

--- app/views/app.tsx
+++ app/views/app.tsx
@@ -2,26 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./../../css/app.css";
 
-const Home = () => (
-  <div>
-    <h2>Reactive Bee</h2>
-    <p>Welcome to Reactive Bee ...</p>
-    <img src="images/rubee.svg" alt="rubee"></img>
-    <nav>
-        <Link to="/">Home</Link><br />
-    </nav>
-  </div>
-);
+import Layout from "./components/layout";
+import Home from "./pages/home";
 
 const NotFound = () => <h2>404 Not Found</h2>;
 
 export function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
